553fa2739fcf308519380b74c208c1d844c76c56
Rocio Kiman <rociokiman@gmail.com>
2020-04-09 12:38:27 -0400

    changed path

--- tests/test_activity_lifetime.py
+++ tests/test_activity_lifetime.py
@@ -8,13 +8,13 @@ from scipy.io.idl import readsav
 
 
 def text_confirm_active_m_dwarfs():
-    path = '../Catalogs/Sources/Douglas2014_praesepe_gaia.csv'
+    path = 'Catalogs/Sources/Douglas2014_praesepe_gaia.csv'
     filepath = pkg_resources.resource_filename(__name__, path)
     douglas2014 = Table.read(filepath)
     
     #Jonathan Gagne run Banyan on this sample for me and here are the
     #results
-    path = '../Catalogs/praesepe_sample_banyan_sigma.sav'
+    path = 'Catalogs/praesepe_sample_banyan_sigma.sav'
     filepath = pkg_resources.resource_filename(__name__, path)
     results = readsav(filepath,verbose=1)
     
