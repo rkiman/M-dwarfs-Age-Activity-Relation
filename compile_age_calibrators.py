218bf69a2e09d31712f4575fcc6e174a35d64a95
Rocio Kiman <rociokiman@gmail.com>
2020-05-04 18:30:36 -0400

    added lines for selecting accretors

--- compile_age_calibrators.py
+++ compile_age_calibrators.py
@@ -23,33 +23,35 @@ they do):
   
 ###--------- Literature search sample ---------###
 
-path = 'Catalogs/literature_search_gaia.fits'
-literature_search1 = Table.read(path)
-
-#Remove haew nans and zeros from the sample. We don't want them!
-mask_nan = ~np.isnan(np.array(literature_search1['ewha']))
-mask_zeros = np.array(literature_search1['ewha'])!=0
-literature_search = literature_search1[mask_nan*mask_zeros]
-
-#Add extinction corrected magnitudes to the sample
-literature_search = src.add_corrected_magnitudes(literature_search)
-
-
-###--------- Select compatible catalogs ---------###
-
 path_compatible = 'Catalogs/literature_search_gaia_compatible.fits'
-if(~os.path.exists(path_compatible)):
-    ls_compatible = src.select_compatible_measurements(literature_search,
-                                                       order=2)
+if(os.path.exists(path_compatible)):
+    ls_compatible = Table.read(path_compatible)
 else:
-    ls_compatible = Table.open(path_compatible)
-
-###--------- M-dwarfs in moving groups ---------###
-
-m_dwarfs_mg,m_dwarfs_not_mg = src.compile_m_moving_groups_sample(ls_compatible)
-
-###--------- M-dwarfs co-moving with a White dwarf ---------###
+    print('No file literature_search_gaia_compatible.fits')
     
+    path = 'Catalogs/literature_search_gaia.fits'
+    literature_search1 = Table.read(path)
+    
+    #Remove haew nans and zeros from the sample. We don't want them!
+    mask_nan = ~np.isnan(np.array(literature_search1['ewha']))
+    mask_zeros = np.array(literature_search1['ewha'])!=0
+    literature_search = literature_search1[mask_nan*mask_zeros]
+    
+    #Add extinction corrected magnitudes to the sample
+    literature_search = src.add_corrected_magnitudes(literature_search)
+    
+    #Select compatible catalogs
+    ls_compatible = src.select_compatible_measurements(literature_search,
+                                                       max_order=2)
+
+#Identify possible accretors
+mask_not_acc = src.identify_accretors(ls_compatible)
+ls_c_not_acc = ls_compatible[mask_not_acc]
+
+#Identify M-dwarfs in moving groups
+m_dwarfs_mg,m_dwarfs_not_mg = src.compile_m_moving_groups_sample(ls_c_not_acc)
+
+#Identify M-dwarfs co-moving with a White dwarf
 m_dwarfs_wd = src.compile_m_wd_sample(m_dwarfs_not_mg)
 
 
@@ -64,8 +66,8 @@ print('Total number of age calibrators: {}'.format(len(age_calibrators)))
 
 lhalbol,lhalbol_error = src.calc_lhalbol(age_calibrators['ewha'],
                                          age_calibrators['ewha_error'],
-                                         age_calibrators['phot_g_mean_mag']-
-                                         age_calibrators['phot_rp_mean_mag'])
+                                         age_calibrators['g_corr']-
+                                         age_calibrators['rp_corr'])
 
 age_calibrators['lhalbol'] = lhalbol
 age_calibrators['lhalbol_error'] = lhalbol_error
