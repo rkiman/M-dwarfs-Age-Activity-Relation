84f87282a5383fc66dd9e0879e22fcd98ce26d52
Rocio Kiman <rociokiman@gmail.com>
2020-10-22 10:15:52 -0400

    Added one main for the compilation of the sample

---
+++ main_create_sample.py
@@ -0,0 +1,40 @@
+
+from astropy.table import Table
+import os
+import src 
+
+#Identify compatible sample
+file = 'Catalogs/literature_search_gaia_compatible.fits'
+if(os.path.exists(file)):
+    literature_search_gaia_compatible = Table.read(file)
+else:
+    literature_search_gaia_compatible = src.compile_compatible_sample()
+
+#Compile sample of calibrators
+file = 'Catalogs/age_calibrators.fits'
+if(os.path.exists(file)):
+    age_calibrators = Table.read(file)
+else:
+    age_calibrators = src.compile_age_calibrators(literature_search_gaia_compatible)
+
+#Identify new members of moving groups
+file = 'data/new_members_data.csv'
+if(os.path.exists(file)):
+    src.find_new_members()
+
+#Make tables for the paper
+dropbox_path = '/Users/rociokiman/Dropbox/Apps/Overleaf/'
+paper = 'Age-Activity Relation for M dwarfs/'
+
+binaries = Table.read('Catalogs/current/wdm_binaries.fits')
+src.make_table_wd_ages(binaries)
+    
+age_calibrators = Table.read('Catalogs/age_calibrators_bayes.fits')
+src.make_table_summary_age_calibrators(age_calibrators)
+    
+accretors = Table.read('Catalogs/literature_search_accretors.fits')
+src.make_table_accretors(accretors)
+
+src.make_table_new_members(dropbox_path,paper)
+
+src.make_table_summary_members(dropbox_path,paper)
